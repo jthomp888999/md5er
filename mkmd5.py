728c8370ce9ff5dd9aba2fd5431b229d5cf1ac92
jthomp888999 <jbthompson1127@yahoo.com>
2016-09-24 10:52:13 -0500

    stuff for testing

---
+++ mkmd5.py
@@ -0,0 +1,21 @@
+import hashlib, sys
+
+file = sys.argv[1]
+
+
+def op_fle(file):
+	try:
+		with open(file) as wd_lst:
+			for word in wd_lst:
+				hsh_lst(word)
+	except IOError:
+		print 'file not found'
+
+
+def hsh_lst(word):
+	word = word.rstrip('\n')
+	hsh_wd = hashlib.md5(word).hexdigest()
+	print "%s | %s" % (hsh_wd, word)
+
+
+op_fle(file)
