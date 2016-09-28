673128fbf9367289c98ee64b470a3182456edf26
jthomp888999 <jbthompson1127@yahoo.com>
2016-09-28 11:43:33 -0500

    keeping everything the same

--- md5er.py
+++ md5er.py
@@ -4,54 +4,59 @@ import hashlib, argparse, sys, time, datetime
 
 def header():
     print """
- +-+-+-+-+-+
- |M|D|5|e|r|
- +-+-+-+-+-+
+    +-+-+-+-+-+
+    |M|D|5|e|r|
+    +-+-+-+-+-+
     """
-# Wordlist used in testing would cause memory read errors
-# open_list() function calls this function for every word
 
-def check_hash(word):
-    # stripping newlines prevents false hashes
-    word = word.rstrip('\n')
+# take the single md5 md5's and the word from the list, compare them after converting words to md5's
+def Mtch_wd(toChk, word):
     hashed = hashlib.md5(word).hexdigest()
-    if (hashed == to_check):
-        print "[+] Hash found %s = %s" % (to_check, word)
-        # I would like to have a message that says the hash couldn't be found
-        # but not repeat for every word that doesn't match
+    if toChk == hashed:
+        return True
 
-def open_list(wordlist):
+# Open the list of words, strip newlines, yield the result to be itterated over
+def Op_lst(wordlist):
     try:
         with open(wordlist) as file:
-            print "[+] Checking known hashes"
             for word in file:
-                # Need to setup this way to avoid MemoryError
-                check_hash(word)
+                word = word.rstrip('\n')
+                yield word
     except IOError:
-        print "[-] File not found"
-    # shouldn't be necessary after some tweaks
-    # except MemoryError:
-        #print "[-] File too large"
-
+        print "[-] Wordlist not found"
     except KeyboardInterrupt:
         print "[-] Check stopped by user"
 
+# Open file with list of hashes, strip newlines from hashes, yield the result to be itterated over
+def Op_hsh(hshFle):
+    try:
+        with open(hshFle) as hshLst:
+            for hshLn in hshLst:
+                hshLn = hshLn.rstrip('\n')
+                yield hshLn
+    except IOError:
+        print '[-] Hash-list not found'
+    except KeyboardInterrupt:
+        print '[-] Check stopped by user'
+
+# creating commandline args, decide how to check hashes weather single or from file
 def main():
+    header()
     # Setting up command-line args (more to come)
     cli_opt = argparse.ArgumentParser(description='A simple MD5 tester.')
-    cli_opt.add_argument('-m', '--md5', required=True, help='Supplies hash to be find')
+    cli_opt.add_argument('-m', '--md5', required=True, help='Supplies hash to be cracked')
     cli_opt.add_argument('-w', '--wordlist', required=True, help='Wordlist to check against')
     args = cli_opt.parse_args()
 
-    # Defining these as global helped splitting up functions but there maybe a cleaner way?
-    global to_check
-    to_check = args.md5
-    global wordlist
-    wordlist = args.wordlist
+    # Wordlist to check against
+    wdLst = args.wordlist
+    toChk = args.md5
+    words = Op_lst(wdLst)
 
-    header()
-    open_list(wordlist)
-
+    for word in words:
+        chkd = Mtch_wd(toChk, word)
+        if chkd == True:
+            print '[+] %s | %s' % (toChk, word)
 
 if __name__ == "__main__":
     main()
